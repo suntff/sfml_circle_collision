ac7987633ba811a23c3f3edb306f0399e55ca1c9
suntff <morakindaniil@gmail.com>
2024-04-24 20:55:02 +0200

    Update Circle.hpp

--- inc/Circle.hpp
+++ inc/Circle.hpp
@@ -1,26 +1,43 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <Game.hpp>
 namespace msp {
 	struct Point {
 		float x; float y;
 	};
-	class Circle{
-		sf::CircleShape shape; float x; float y; float r; float v; float alfa; int n;
+	class Circle {
+		float x; float y; sf::CircleShape shape; int width; int height;
+		sf::Vector2f velocity;
 	public:
-		Circle() = default;
-		Circle(Point p1, float r, float v, float alfa, int n);
-		void Setup(Point p1, float r, float v, float alfa,int n);
-		sf::CircleShape Get();
-		void Move(float dt);
-		float X() { return x; }
-		float Y() { return y; }
-		float R() { return r; }
-		float Alfa() { return alfa; }
-		void Alfa(float alfa) {
-			this->alfa = alfa;
+		Circle(float x, float y, float vx, float vy, float radius,int width,int height) : velocity(vx, vy), width(width), height(height) {
+			this->x = x; this->y = y;
+			shape.setRadius(radius);
+			shape.setOrigin(radius, radius);
+			shape.setPosition(x, y);
+			shape.setFillColor(sf::Color::Red);
+
 		}
-		void Inverse_V() {
-			v = -v;
+		void update(float dt) {
+			shape.move(velocity * dt);
+
+			if (shape.getPosition().x - shape.getRadius() < 0 || shape.getPosition().x + shape.getRadius() > width) {
+				velocity.x *= -1;
+			}
+
+			if (shape.getPosition().y - shape.getRadius() < 0 || shape.getPosition().y + shape.getRadius() > height) {
+				velocity.y *= -1;
+			}
+		}
+		sf::CircleShape Circle::Get(){
+			return shape;
+		}
+		sf::Vector2f& Velocity(){
+			return velocity;};
+		const sf::Vector2f& Circle::getPosition() const {
+			return shape.getPosition();
+		}
+		float	Circle::getRadius() const {
+			return shape.getRadius();
 		}
 	};
 }
